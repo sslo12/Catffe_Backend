7b9ab42e19aab54581bd703edeefd3dfd5b31ead
Shirley <115416417+sslo12@users.noreply.github.com>
2023-10-26 23:43:01 -0500

    Add files via upload

---
+++ Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/dominio/VentasDTO.java
@@ -0,0 +1,22 @@
+package com.catffe.apiweb.dominio;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VentasDTO {
+    private int id;
+    private Date fecha_hora;
+    private int cliente_id;
+    private int trabajador_id;
+    private double precio_total;
+    private List<Map<String, Integer>> detalles;
+}
+
