98651cbb31cf68cee45c13157225116adfd77a2a
Shirley <115416417+sslo12@users.noreply.github.com>
2023-10-26 23:40:45 -0500

    Update VentaServiceImp.java

--- Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/service/VentaServiceImp.java
+++ Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/service/VentaServiceImp.java
@@ -1,6 +1,5 @@
 package com.catffe.apiweb.service;
 
-import com.catffe.apiweb.model.VentaModel;
 import com.catffe.apiweb.model.VentasModel;
 import com.catffe.apiweb.repository.IVentasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,27 +10,29 @@ import java.util.Optional;
 
 @Service
 public class VentaServiceImp implements IVentaService {
+
     @Autowired
     IVentasRepository ventasRepository;
 
     @Override
-    public String crearVenta(VentaModel venta) {
+    public String guardarVenta(VentasModel venta) {
         ventasRepository.save(venta);
         return "La venta con el id: " + venta.getId() + " fue creada exitosamente";
     }
-
     @Override
     public List<VentasModel> listarVentas() {
         return ventasRepository.findAll();
     }
 
     @Override
-    public Optional<VentasModel> obtenerVentaPorId(String ventaId) {
+    public Optional<VentasModel> obtenerVentaPorId(int ventaId) {
         return ventasRepository.findById(ventaId);
     }
 
     @Override
-    public void eliminarVentaPorId(String ventaId) {
+    public String eliminarVentaPorId(int ventaId) {
         ventasRepository.deleteById(ventaId);
+        return "La venta con el id: "+ventaId+" fue eliminada exitosamente";
     }
+
 }
