2160bcb8aac31a4e2c6dce1dd812eef9a6d5373c
Shirley <115416417+sslo12@users.noreply.github.com>
2023-10-27 08:47:49 -0500

    Update TrabajadorController.java

--- Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/controller/TrabajadorController.java
+++ Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/controller/TrabajadorController.java
@@ -1,12 +1,12 @@
 package com.catffe.apiweb.controller;
 
 import com.catffe.apiweb.exception.RecursoNoEncontradoException;
-import com.catffe.apiweb.model.ProductosModel;
-import com.catffe.apiweb.model.VentaModel;
-import com.catffe.apiweb.service.IProductosService;
-import com.catffe.apiweb.service.IVentaService;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
+import com.catffe.apiweb.model.enums.TipoUsuario;
+import com.catffe.apiweb.model.TrabajadorModel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.catffe.apiweb.service.ITrabajadorService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,26 +15,80 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/catffe/ventas")
-
+@RequestMapping("/catffe/Trabajador")
 public class TrabajadorController {
+
     @Autowired
-    private IVentaService ventaService;
-    @Autowired
-    private IProductosService productosService;
+    ITrabajadorService trabajadorService;
 
     @PostMapping("/insert")
-    public ResponseEntity<String> crearTrabajador(@RequestBody TrabajadorModel trabajador){
-        // Documento de credenciales
-        DBObject nuevaCredencial = new BasicDBObject();
-        nuevaCredencial.put("usuario",trabajador.getUsuario());
-        if (trabajador.getTipoTrabajador()==1)
-            nuevaCredencial.put("tipoUsuario", TipoUsuario.ADMIN);
-        else
-            nuevaCredencial.put("tipoUsuario", TipoUsuario.EMPLEADO;
-        nuevaCredencial.put("contrasena", trabajador.getContrasena());
-        trabajador.setCredenciales(nuevaCredencial);
-        trabajadorService.guardarTrabajador(trabajador);
-        return new ResponseEntity<String>(trabajadorService.guardarTrabajador(trabajador), HttpStatus.OK);
+    public ResponseEntity<String> crearTrabajadores(@RequestBody List<TrabajadorModel> trabajadores) {
+        for (TrabajadorModel trabajador : trabajadores) {
+            TrabajadorModel.Credenciales credenciales = new TrabajadorModel.Credenciales();
+            credenciales.setUsuario(trabajador.getCredenciales().getUsuario());
+            credenciales.setTipoUsuario(trabajador.getCredenciales().getTipoUsuario());
+            credenciales.setContrasena(trabajador.getCredenciales().getContrasena());
+
+            trabajador.setCredenciales(credenciales);
+            trabajadorService.guardarTrabajador(trabajador);
+        }
+
+        return new ResponseEntity<>("Trabajadores creados exitosamente", HttpStatus.OK);
     }
-}
+
+
+    @GetMapping("/all/list")
+    public ResponseEntity<List<TrabajadorModel>>mostrarTrabajadores(){
+        return new ResponseEntity<List<TrabajadorModel>>(trabajadorService.ListarTrabajadores(),HttpStatus.OK);
+    }
+
+    @GetMapping ("/find/{id}")
+    public ResponseEntity<TrabajadorModel>buscarTrabajadorPorId(@PathVariable int id){
+        TrabajadorModel Trabajador = trabajadorService.obtenerTrabajadorporID(id).
+                orElseThrow(()->new RecursoNoEncontradoException("Error! no se encontro el cliente con el id "+id));
+
+        return ResponseEntity.ok(Trabajador);
+    }
+
+
+    @DeleteMapping ("/delete/{id}")
+    public ResponseEntity<String>eliminarTrabajadorPorId(@PathVariable int id){
+        TrabajadorModel Trabajador = trabajadorService.obtenerTrabajadorporID(id).
+                orElseThrow(()-> new RecursoNoEncontradoException("Error! no se encontro el cliente con el id "+id));
+
+        return new ResponseEntity<String>(trabajadorService.eliminarTrabajadorPorID(Trabajador.getId()),HttpStatus.OK);
+    }
+
+
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> actualizarTrabajador(@PathVariable int id, @RequestBody TrabajadorModel detallesTrabajador) {
+        TrabajadorModel trabajador = trabajadorService.obtenerTrabajadorporID(id)
+                .orElseThrow(() -> new RecursoNoEncontradoException("Error! no se encontró el trabajador con el id " + id));
+
+        // Validación de campos
+        String nombreTActualizar = detallesTrabajador.getNombre();
+        String apellidoTActualizar = detallesTrabajador.getApellido();
+        TrabajadorModel.Credenciales credencialesTActualizar = detallesTrabajador.getCredenciales();
+
+        if (nombreTActualizar == null || nombreTActualizar.trim().isEmpty() ||
+                apellidoTActualizar == null || apellidoTActualizar.trim().isEmpty()) {
+            throw new RecursoNoEncontradoException("Error! Asegúrate de que el nombre y el apellido no estén vacíos.");
+        }
+
+        // Asignamos los valores que vamos a actualizar del trabajador
+        trabajador.setNombre(nombreTActualizar);
+        trabajador.setApellido(apellidoTActualizar);
+
+        // Actualizamos las credenciales si no son nulas y no están vacías
+        if (credencialesTActualizar != null &&
+                credencialesTActualizar.getUsuario() != null && !credencialesTActualizar.getUsuario().trim().isEmpty() &&
+                credencialesTActualizar.getContrasena() != null && !credencialesTActualizar.getContrasena().trim().isEmpty()) {
+            trabajador.setCredenciales(credencialesTActualizar);
+        }
+
+        // Guardamos los cambios
+        trabajadorService.actualizarTrabajador(trabajador);
+        return new ResponseEntity<>("El trabajador con el id: " + id + " fue actualizado exitosamente", HttpStatus.OK);
+    }
+    }
