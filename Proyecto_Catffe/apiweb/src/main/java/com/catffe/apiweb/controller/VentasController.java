4432991e898e3b04041f9c41233cb2ded8e4439b
Shirley <115416417+sslo12@users.noreply.github.com>
2023-10-26 23:38:06 -0500

    Update VentasController.java

--- Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/controller/VentasController.java
+++ Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/controller/VentasController.java
@@ -1,117 +1,143 @@
 package com.catffe.apiweb.controller;
 
+import com.catffe.apiweb.dominio.VentasDTO;
+import com.catffe.apiweb.exception.RecursoNoEncontradoException;
 import com.catffe.apiweb.model.ProductosModel;
-import com.catffe.apiweb.model.VentaModel;
+import com.catffe.apiweb.model.VentasModel;
 import com.catffe.apiweb.service.IProductosService;
 import com.catffe.apiweb.service.IVentaService;
-import com.mongodb.DBObject;
-import org.springframework.beans.factory.annotation.Autowired;
+import  org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/catffe/ventas")
-public class VentaController<Producto, Productos> {
+public class VentasController {
 
     @Autowired
     private IVentaService ventaService;
+
     @Autowired
-    private IProductosService productoService;
+    IProductosService productosService;
 
     @PostMapping("/insert")
-    public ResponseEntity<String> crearVenta(@RequestBody VentaModel venta) {
-        // Verificar si el producto está disponible
-        boolean productoDisponible = productoService.verificarDisponibilidad(venta.getDetalles());
+    public ResponseEntity<String> crearVenta(@RequestBody VentasDTO ventadto) {
+        VentasModel ventas = new VentasModel ();
+        ventas.setId(ventadto.getId());
+        ventas.setFecha_hora(ventadto.getFecha_hora());
+        ventas.setCliente_id(ventadto.getCliente_id());
+        ventas.setTrabajador_id(ventadto.getTrabajador_id());
 
-        if (productoDisponible) {
-            // Restar productos vendidos del stock
-            productoService.restarProductos(venta.getDetalles());
+        //Garantizar que existan productos y cantidades del detalle de la venta, si existen
+        List<Map<String, Integer>> detalles = ventadto.getDetalles();
+        ProductosModel producto = new ProductosModel();
+        int IdProducto;
+        int cantidad;
+        for (int i = 0 ; i < detalles.size() ; i++) {
+            IdProducto = detalles.get(i).get("id_producto");
+            producto = this.productosService.obtenerProductoPorId(IdProducto).orElseThrow(()-> new RecursoNoEncontradoException("El producto no existe") );
+            cantidad = detalles.get(i).get("cantidad");
+                    if (producto != null && cantidad <= producto.getCantidad_dispo()) {
 
-            // Guardar venta
-            String result = ventaService.crearVenta(venta);
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("El producto no está disponible", HttpStatus.BAD_REQUEST);
-        }
-    }
+                        //En el objeto de detalle adicionar el id de Venta
+                        int CantidadActual = producto.getCantidad_dispo();
+                        double productoPrecio = producto.getPrecio();
 
+                        producto.setCantidad_dispo(CantidadActual - cantidad);
+                        productosService.actualizarProducto(producto);
 
+                        double totalPrecioProducto = productoPrecio * cantidad;
+                        double TotalPrecioActual = ventadto.getPrecio_total();
 
+                        ventadto.setPrecio_total(TotalPrecioActual + totalPrecioProducto);
+                        ventadto.setPrecio_total(ventadto.getPrecio_total());
+                        ventaService.guardarVenta(ventas);
 
+                        ProductosModel nuevodetalle = new ProductosModel();
 
+                        nuevodetalle.setId(producto.getId());
+                        nuevodetalle.setCantidad_dispo(CantidadActual);
 
-
-
-
-
-
-
-
-
-
-    @PostMapping("/all/insertar")
-    public String insertarVenta(@RequestBody VentaModel venta) {
-        try {
-            // Paso 1: Obtener productos actuales
-            Map<Integer, Producto> productosActuales = new HashMap<>();
-            for (DBObject detalle : venta.getDetalles()) {
-                int idProducto = (int) detalle.get("id_producto");
-                Producto productoActual = (Producto) productoService.obtenerProductoPorId(idProducto);
-                if (productoActual == null) {
-                    return "El producto con ID " + idProducto + " no existe.";
+                        System.out.print("id_producto " + IdProducto);
+                        System.out.print("cantidad " + cantidad);
+                        System.out.print("precio_total " + ventadto.getPrecio_total());
+                    }
                 }
-                productosActuales.put(idProducto, productoActual);
+                // De shoppingCart recuperar cada detalle de compra
+                // Si no existen productos o cantidades del detalle de la venta, retornar un error
+                return new ResponseEntity<String>(ventaService.guardarVenta(ventas),HttpStatus.OK);
             }
 
-            // Paso 2: Actualizar las cantidades
-            for (DBObject detalle : venta.getDetalles()) {
-                int idProducto = (int) detalle.get("id_producto");
-                detalle.get("nombre");
-                int cantidad = (int) detalle.get("cantidad");
-                Producto productoActual = productosActuales.get(idProducto);
+    /*@PostMapping("/insert")
+    public ResponseEntity<String> crearVenta(@RequestBody VentasDTO ventadto) {
+        VentasModel ventas = new VentasModel();
+        ventas.setId(ventadto.getId());
+        ventas.setCliente_id(ventadto.getCliente_id());
+        ventas.setTrabajador_id(ventadto.getTrabajador_id());
 
-                if (productoActual.getCantidad() < cantidad) {
-                    return "El producto con ID " + idProducto + " no tiene suficiente stock para completar la venta.";
-                }
+        // Inicializar el precio total en 0
+        double precioTotal = 0.0;
+        int IdProducto = 0;
+        int cantidad = 0;
 
-                productoActual.setCantidadDipo(productoActual.get() - cantidad);
-                productoService.actualizarProducto((ProductosModel) productoActual);
+        // Garantizar que existan productos y cantidades del detalle de la venta, si existen
+        List<Map<String, Integer>> detalles = ventadto.getDetalles();
+        for (Map<String, Integer> detalle : detalles) {
+            IdProducto = detalle.get("id_producto");
+            cantidad = detalle.get("cantidad");
+
+            // Buscar el producto por ID
+            ProductosModel producto = productosService.obtenerProductoPorId(IdProducto)
+                    .orElseThrow(() -> new RecursoNoEncontradoException("El producto no existe"));
+
+            // Verificar si hay suficiente cantidad disponible
+            if (cantidad < producto.getCantidad_dispo()) {
+                double productoPrecio = producto.getPrecio();
+
+                // Actualizar el inventario del producto
+                producto.setCantidad_dispo(producto.getCantidad_dispo() - cantidad);
+                productosService.actualizarProducto(producto);
+
+                // Calcular el precio total para este producto en el detalle de venta
+                double totalPrecioProducto = productoPrecio * cantidad;
+                precioTotal += totalPrecioProducto;}
+            else {
+                // Si no hay suficiente cantidad disponible, devolver un error
+                return new ResponseEntity<>("No hay suficiente cantidad disponible para el producto con ID " + IdProducto, HttpStatus.BAD_REQUEST);
             }
-
-            // Paso 3: Guardar la venta
-            ventaService.crearVenta(venta);
-
-            return "Venta realizada con éxito.";
         }
-    }
 
-    @GetMapping("/all/list")
-    public ResponseEntity<List<VentasModel>> mostrarVentas() {
+        // Asignar el precio total calculado a la venta
+        ventas.setPrecio_total(precioTotal);
+
+        // Guardar la venta en la base de datos
+        String resultado = ventaService.guardarVenta(ventas);
+        return new ResponseEntity<>(resultado, HttpStatus.OK);
+    }*/
+
+    @GetMapping("/todas/listar")
+    public ResponseEntity<List<VentasModel>>listarVentas(){
         return new ResponseEntity<List<VentasModel>>(ventaService.listarVentas(),HttpStatus.OK);
     }
 
-    @GetMapping("/find/{id}")
-    public ResponseEntity<VentasModel> buscarVentaPorId(@PathVariable String id) {
-        VentasModel venta = ventaService.obtenerVentaPorId(id)
-                .orElseThrow(() -> new RecursoNoEncontradoException("Error: no se encontró venta con el ID " + id));
+    @GetMapping ("/find/{id}")
+    public ResponseEntity<VentasModel>obtenerVentaPorId(@PathVariable int id){
+        VentasModel venta = ventaService.obtenerVentaPorId(id).
+                orElseThrow(()->new RecursoNoEncontradoException("Error: no se encontró la venta con el ID " +id));
 
         return ResponseEntity.ok(venta);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> eliminarVentaPorId(@PathVariable String id) {
-        VentasModel venta = ventaService.obtenerVentaPorId(id)
-                .orElseThrow(() -> new RecursoNoEncontradoException("Error: no se encontró la venta con el ID " + id));
+    @DeleteMapping ("/delete/{id}")
+    public ResponseEntity<String>eliminarVentaPorId(@PathVariable int id){
+        VentasModel venta = ventaService.obtenerVentaPorId(id).
+                orElseThrow(()-> new RecursoNoEncontradoException("Error: no se encontró la venta con el ID " +id));
 
-        ventaService.eliminarVentaPorId(id);
-
-        return new ResponseEntity<>("La venta con el ID " + id + " ha sido eliminada exitosamente", HttpStatus.OK);
+        return new ResponseEntity<String>(ventaService.eliminarVentaPorId(venta.getId()),HttpStatus.OK);
     }
+
 }
