5c58713e7079eaff76246c0ab9e8df900fcce38a
Shirley <115416417+sslo12@users.noreply.github.com>
2023-10-26 23:38:44 -0500

    Update VentaModel.java

--- Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/model/VentaModel.java
+++ Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/model/VentaModel.java
@@ -1,27 +1,23 @@
 package com.catffe.apiweb.model;
 
-import com.mongodb.DBObject;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import java.util.ArrayList;
+
 import java.util.Date;
-import java.util.List;
+
 
 @Document("venta")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class VentaModel {
-
+public class VentasModel {
     @Id
-    private String _id;
+    private int id;
     private Date fecha_hora;
     private int cliente_id;
     private int trabajador_id;
-    private List<DBObject> detalles = new ArrayList<>();
     private double precio_total;
-
 }
