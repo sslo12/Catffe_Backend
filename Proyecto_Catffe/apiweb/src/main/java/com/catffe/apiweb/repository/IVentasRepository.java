f84cdc7e7a2e379aa60492f304684b9f8863b22b
Shirley <115416417+sslo12@users.noreply.github.com>
2023-10-27 22:52:32 -0500

    Update IVentasRepository.java

--- Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/repository/IVentasRepository.java
+++ Proyecto_Catffe/apiweb/src/main/java/com/catffe/apiweb/repository/IVentasRepository.java
@@ -3,7 +3,9 @@ package com.catffe.apiweb.repository;
 import com.catffe.apiweb.model.VentasModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IVentasRepository extends MongoRepository<VentasModel, Integer> {
+      List<VentasModel> findByTrabajadorIdOrderByFechaHoraAsc(int trabajadorId);
 }
